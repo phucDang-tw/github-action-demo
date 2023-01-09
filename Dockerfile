a76d6a2fcf7b44ced824ed6336939c5b2e37906c
Pee Tankulrat <{ID}+{username}@users.noreply.github.com>
2023-01-09 09:54:50 +0700

    Deploy to test

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM node:alpine-lts
+
+COPY server.js server.js
+
+CMD node server.js
---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require("http");
+
+console.log("Server is running");
+//create a server object:
+http
+  .createServer(function (req, res) {
+    res.write(`
+  ${process.env.SOME_CONFIG}
+  ${process.env.SOME_SECRET}
+  `); //write a response to the client
+    res.end(); //end the response
+  })
+  .listen(8080); //the server object listens on port 8080
